c44a10581e98161a9a0b583c9ff93b9d7cb67b6a
matt.gummel <mgummel25@gmail.com>
2020-09-01 22:49:27 -0700

    added modeladmin view

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 
 from .models import Post, Category
 
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
